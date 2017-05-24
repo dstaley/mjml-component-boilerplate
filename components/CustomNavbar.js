735c10c21768658a942cb8692ec9e67716eb0974
Dylan Staley <staley.dylan@gmail.com>
2017-05-24 11:05:35 -0500

    add custom navbar example

---
+++ components/CustomNavbar.js
@@ -0,0 +1,28 @@
+import { MJMLElement } from 'mjml-core';
+import React, { Component } from 'react';
+import Column from 'mjml-column';
+import { Navbar, InlineLinks, Link } from 'mjml-navbar';
+
+@MJMLElement
+class CustomNavbar extends Component {
+    render() {
+        return (
+            <Navbar>
+                <Column>
+                    <InlineLinks>
+                        <Link href="/one" key="/one">One</Link>
+                        <Link href="/two" key="/two">Two</Link>
+                        <Link href="/three" key="/three">Three</Link>
+                    </InlineLinks>
+                </Column>
+            </Navbar>
+        );
+    }
+}
+
+CustomNavbar.tagName = 'custom-navbar';
+CustomNavbar.parentTag = ['mj-container'];
+CustomNavbar.endingTag = false;
+CustomNavbar.defaultMJMLDefinition = { content: '' };
+
+export default CustomNavbar;
